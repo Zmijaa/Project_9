aa73911f2fbba6d589e10bd59860d435a9b9e3ab
Zmijaa <mircevfilip98@gmail.com>
2023-11-07 13:23:32 +0100

    first commit

---
+++ src/CvoF.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class CvoF {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner input=new Scanner(System.in);
+int c,f;
+System.out.println("Vnesi broj na celzjusoi stepeni");
+c=input.nextInt();
+f=9*c/5+32;
+System.out.println("Temperaturata vo farenhajt e "+f);
+
+
+
+	}
+
+}
